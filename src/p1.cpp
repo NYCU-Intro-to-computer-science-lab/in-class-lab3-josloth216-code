ea78bbb43833d877b687e56495a07e90ce26ca9d
josloth216-code <josloth216@gmail.com>
2025-11-25 14:39:32 +0800

    Update p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,18 +1,16 @@
-// 題目 1：高塔建築師 (The Tower Architect) - 20分
-
 #include <bits/stdc++.h>
 using namespace std;
-int bb(int k){
+long long bb(int k){
   if(k==1){return 1;}
   else return(k*bb(k-1));
 }
-int aa(int k){
+long long aa(int k){
   if(k==1){return 1;}
   else return(k+aa(k-1));
 }
 
 int main() {
-  int x;
+  long long x;
   cin>>x;
   cout<<bb(x) << endl<<aa(x);
 }
