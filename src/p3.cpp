fc38eab0187472861945fb13c88dbd275860c9a7
josloth216-code <josloth216@gmail.com>
2025-11-25 13:55:58 +0800

    Update p3.cpp

--- src/p3.cpp
+++ src/p3.cpp
@@ -1,10 +1,34 @@
 // 題目 3：破解數學家的謎題 (Cracking the Mathematician's Puzzle) - 30分
 
 #include <iostream>
+
 using namespace std;
 
-int main() {
-    
-    
-    return 0;
+bool isPrime(int n){
+  for(int i=1;i<=n;i++){
+    if(n%i==0 && i!=1 && i!=n){
+      return false;
+    }
+  }
+  return true;
+}
+
+int main(){
+  int a,x;
+  bool s=false;
+  cin>>a;
+  for(int i=2;i<(a/2)-1;i++){
+    if(s==true){break;}
+    if(isPrime(i)==true){
+      x=i;
+      for(int k=x;k<a;k++){
+      if(isPrime(k)==true && x+k==a){
+        cout<<x<<" "<<k;
+        s=true;
+        break;
+      }
+      }
+    }
+  }
+  return 0;
 }
