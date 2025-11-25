a3759e13e773d39f64569d0e22a04b42ca35942b
josloth216-code <josloth216@gmail.com>
2025-11-25 13:35:51 +0800

    Update p2.cpp

--- src/p2.cpp
+++ src/p2.cpp
@@ -1,10 +1,21 @@
 // 題目 2：完美鋪磚計畫 (The Perfect Tiling Project) - 20分
 
 #include <iostream>
+
 using namespace std;
 
-int main() {
-    
-    
-    return 0;
+int GCD(int num1,int num2)
+{
+	if(num2==0)
+	{
+	 return num1;
+	} 
+	
+	return GCD(num2,num1%num2);
+}
+int main(){
+  int x,y;
+  cin>>x>>y;
+  cout<<GCD(x,y);
+  return 0;
 }
